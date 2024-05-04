fb5a9b2769f7d8cacd08e6c20210de6d9ed81e59
ayaz-alijamali <ayazalijamali64@gmail.com>
2024-05-04 22:18:08 +0500

    100 days coding challenge Questions

---
+++ index.js
@@ -0,0 +1,16 @@
+// Questoin __________ 100
+let number = 144;
+let squareRoot = Math.sqrt(number);
+console.log("Square root of", number, "is", squareRoot);
+// Question ____________ 101
+// Generate a random decimal number between 0 and 1
+let randomDecimal = Math.random();
+// Scale the random decimal number to the desired range (1 to 10)
+let randomNumber = Math.floor(randomDecimal * 10) + 1;
+console.log("Random number between 1 and 10:", randomNumber);
+// Question ____________ 102
+const num1 = -5;
+const num2 = 5;
+const absoluteDifference = Math.abs(num1 - num2);
+console.log(`The absolute difference between ${num1} and ${num2} is ${absoluteDifference}`);
+export {};
